2d5cf3333dc3e1bb264d03394e630c759fd3828a
arkeks <theincrediblelynx@yandex.ru>
2021-11-20 16:03:01 +0300

    Made newlines, corrected printed comms.

--- project/src/main.c
+++ project/src/main.c
@@ -2,12 +2,14 @@
 
 int main(void) {
     Matrix* test_matrix = create_matrix(3, 3);
+
     for (size_t i = 0; i < test_matrix -> rows; ++i) {
         for (size_t g = 0; g < test_matrix -> cols; ++g) {
             test_matrix->matrix[i][g] = i + g;
         }
     }
 
+    printf("test matrix:\n");
     for (size_t i = 0; i < test_matrix -> rows; ++i) {
         for (size_t g = 0; g < test_matrix -> cols; ++g) {
             printf("%lf ", test_matrix -> matrix[i][g]);
@@ -15,28 +17,33 @@ int main(void) {
         printf("\n");
     }
     printf("\n");
+
     Matrix* test_minor;
-    printf("now getting min\n");
     test_minor = get_minor_matrix(test_matrix, 0, 0);
 
+    printf("minor for test matrix:\n");
     for (size_t i = 0; i < test_minor -> rows; ++i) {
         for (size_t g = 0; g < test_minor -> cols; ++g) {
             printf("%lf ", test_minor -> matrix[i][g]);
         }
         printf("\n");
     }
-
+    printf("\n");
+    
     Matrix* test_adj;
     test_adj = adj(test_matrix);
+    printf("adj matrix for test matrix:\n");
 
     for (size_t i = 0; i < test_adj -> rows; ++i) {
         for (size_t g = 0; g < test_adj -> cols; ++g) {
-            printf("adj %lf ", test_adj -> matrix[i][g]);
+            printf("%lf ", test_adj -> matrix[i][g]);
         }
         printf("\n");
     }
+
     free_matrix(test_matrix);
     free_matrix(test_minor);
     free_matrix(test_adj);
+
     return 0;
 }
